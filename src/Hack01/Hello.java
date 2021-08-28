bf2d49268d7647e48b887eae56db8eb456048399
Owner <Owner@DESKTOP-CRS5LBJ.unl.edu>
2021-08-28 15:13:54 -0600

    Added Partner Details

--- src/Hack01/Hello.java
+++ src/Hack01/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Ava Blecha!\n");
 		System.out.println("Computer Science\n");
+		System.out.print("Elijah Harvoy, "
+				+ "also Computer Science\n");
 	}
 }
